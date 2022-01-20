5d0d73850e9a4711de3be84e2d629ba435cbcac7
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-20 10:13:12 +0700

    gg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
+import "./App.css";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={() => {
+        i18n.changeLanguage("vi");
+      }}>vi</button>
+      <button onClick={() => {
+        i18n.changeLanguage("en");
+      }}>en</button>
+      <div></div>
+      {t("hello.2")}
+      <div></div>
+      {t("good bye")}
+      <div></div>
+      {t("thanks")}
+      <div></div>
+      {t("hello.1")}
+
     </div>
   );
 }
